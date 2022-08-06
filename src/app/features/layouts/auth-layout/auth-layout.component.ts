1d4a16d9eb2c8814a712fabda72eba5dae05c536
Andrei Petrov <68751397+ndt080@users.noreply.github.com>
2022-08-06 18:31:41 +0300

    fix: auth markup

--- src/app/features/layouts/auth-layout/auth-layout.component.ts
+++ src/app/features/layouts/auth-layout/auth-layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from '@shared/animations/slide-in.animation';
 
 @Component({
   selector: 'auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.scss'],
+  animations: [slideInAnimation],
 })
 export class AuthLayoutComponent {}
