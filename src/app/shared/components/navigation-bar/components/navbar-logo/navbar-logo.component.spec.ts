e89c6cf8ffeaaf49391c951a8fa68d6bc4c319d9
Andrei Petrov <68751397+ndt080@users.noreply.github.com>
2022-09-28 12:58:37 -0700

    feat: refactor navbar markup

---
+++ src/app/shared/components/navigation-bar/components/navbar-logo/navbar-logo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarLogoComponent } from './navbar-logo.component';
+
+describe('NavbarLogoComponent', () => {
+  let component: NavbarLogoComponent;
+  let fixture: ComponentFixture<NavbarLogoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarLogoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarLogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
